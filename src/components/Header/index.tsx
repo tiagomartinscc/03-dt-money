6dac2633d012a7c9ab6e473890ccb9a75f75c8fb
Tiago Martins <tiagomartinscc@hotmail.com>
2024-09-04 23:15:11 -0300

    styling a modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,23 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logoImg from "../../assets/logo.svg"
 import * as Dialog from '@radix-ui/react-dialog';
 
-export function Header () {
+import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
+
+export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={logoImg} alt="Logo" />
-        <Dialog.Root>
-          <Dialog.Trigger asChild>
-            <NewTransactionButton>Nova Transação</NewTransactionButton>
-          </Dialog.Trigger>
-          <Dialog.Portal>
-            <Dialog.Overlay />
-            <Dialog.Content>
-              <Dialog.Title>Nova Transação</Dialog.Title>
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
-        </Dialog.Root>
+        <img src={logoImg} alt="" />
+
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <NewTransactionButton>Nova transação</NewTransactionButton>
+        </Dialog.Trigger>
+
+        <NewTransactionModal />
+      </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
-  )
+  );
 }
