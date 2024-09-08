6a0602ce95c1fef741faea9d752dd450b6000b14
Tiago Martins <tiagomartinscc@hotmail.com>
2024-09-08 11:40:23 -0300

    separating the summary logic into a hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,21 +1,10 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
-import { useContext } from "react";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { numberFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary () {
-  const {transactions} = useContext(TransactionsContext)
-
-  const summary = transactions.reduce((acc, transaction) => {
-    if (transaction.type === 'income') {
-      acc.income += transaction.price
-    } else {
-      acc.outcome += transaction.price
-    }
-    acc.total = acc.income - acc.outcome
-    return acc
-  }, {income: 0, outcome: 0, total: 0})
+  const summary = useSummary()
   return (
     <SummaryContainer>
       <SummaryCard>
